945ca37692e0c664197e9f20fc492f82d5465c15
Aaron Brady <abrady@fb.com>
2014-12-19 13:46:36 -0800

    tweaks

--- PlatformBootcampParse/public/canvas.js
+++ PlatformBootcampParse/public/canvas.js
@@ -3,15 +3,20 @@
 
 var FBInfo = React.createClass({
   getInitialState: function() {
-    return { authResponse: {} };
+    return { authResponse: null };
   },
 
   render: function() {
     var logged_in;
-    if(this.getState().authResponse) {
-      logged_in = <div>Logged in!</div>;
+    if(this.state.authResponse) {
+      console.log('auth response!');
+      logged_in = <div>
+        <FBName/>
+        <FBShare/>
+      </div>;
     } else {
-      logged_in = <div>Not Logged In</div>;
+      console.log('no auth response');
+      logged_in = <div></div>;
     }
     return (
       <div className="large-12 columns">
@@ -21,8 +26,62 @@ var FBInfo = React.createClass({
   }
 });
 
-var fbInfo = React.renderComponent(<FBInfo/>, document.getElementById('fb_area') );
+var FBName = React.createClass({
+  getInitialState: function() {
+    console.log('state');
+    return { name: '' };
+  },
+  onGraphResponse: function(res){
+    if(res.name) {
+      this.setState({name: res.name});
+    } else {
+      console.log('api(/me) failed '+JSON.stringify(res));
+    }
+  },
+  componentWillMount: function() {
+    console.log('mount');
+    FB.api('/me',this.onGraphResponse);
+  },
+  render: function() {
+    console.log('render');
+    if(!this.state.name) {
+      return <div/>;
+    }
+    return (
+      <div className="large-4 medium-4 columns">
+        Hello {this.state.name}!
+      </div>
+    );
+  }
+});
+
+var FBShare = React.createClass({
+  onShareResponse: function(response) {
+    console.log('share response '+JSON.stringify(response));
+  },
+  shareLink: function() {
+    FB.ui(
+      {method: 'share', href: 'https://developers.facebook.com/docs/'}, 
+      this.onShareResponse
+    );
+  },
+  render: function() {
+    return (
+      <div className="row">
+        <div className="small-12 columns">
+          <button className="column small small-2 radius button" onClick={this.shareLink}>
+            Share
+          </button>
+        </div>
+        <hr/>
+      </div>
+    );
+  }
+});
+
+var fbInfo = React.renderComponent(
+    <FBInfo/>, document.getElementById('fb_area') );
 
 function onFBLogin(response) {
-  fbInfo.setState({authResponse: response});
+  fbInfo.setState({authResponse: response.authResponse});
 }
